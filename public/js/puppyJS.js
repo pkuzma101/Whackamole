6bbb0fb8ef4e675790b83e40473e82e89b31e5e4
pkuzma101 <kuzma.paul@gmail.com>
2014-11-07 11:32:11 -0600

    Paul Kuzma's Whack-a-Mole game

---
+++ public/js/puppyJS.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+// Score begins at 0
+var score = 0;
+// Links the 'scoreboard' tag to the JS document
+var gameScore = document.getElementById('scoreboard');
+
+
+function fadeInPuppy() {
+	// Grabs the images inside each individual square
+	var square = $('.col-sm-4').children();
+	// Code for choosing a random square
+	var rand = Math.floor(Math.random()* square.length);
+	// Picks a random square and fades the puppy in
+	var puppy = $(square[rand]).fadeIn();
+	// Keeps it from adding points based on how many times puppy has appeared
+	$(this).off();
+	// Each puppy will fade out after three seconds
+	setTimeout(function() {
+		$(puppy).fadeOut();
+		}, 3000);
+	// The following effects will happen when a puppy is clicked...
+	$(puppy).click(function() {
+		// Calls the whimper sound from the html
+		var puppyWhimper = document.getElementById('whimper');
+		// Loads the sound
+		puppyWhimper.load();
+		// Plays the sound
+		puppyWhimper.play();
+		// Keeps game from adding points based on how many times this puppy is clicked on
+		$(this).off();
+		// Adds one point per click
+		score++;
+		// Puppy fades upon being clicked
+		$(this).fadeOut();
+	});
+	// Makes player's score visible on the page
+	gameScore.innerHTML = score;
+}
+		
+function countDown() {
+	var timeLeft = document.getElementById('timer');
+	    // You have 45 seconds to pet all puppies
+	    var gameTimer = 45; // seconds
+	    // Exactly one second between count downs
+	    var interval = 1000;
+	    // Establishes what to do during countdown
+	    var intervalId = setInterval(function () {
+	        if(gameTimer == 0) {
+	            clearInterval(intervalId);
+	            // Announces final score
+	            alert("Done!  Your score is " + score + "!");
+	        }
+	        else {
+	        	// Timer counts down one by one
+	            gameTimer--;
+	            // Establishes new time
+	            timeLeft.innerHTML = gameTimer;
+	            // Calls the function that fades in the puppies and counts the score
+	            fadeInPuppy();
+	        }
+	        // Counts down every one second
+	        }, interval);
+	}
+
+function hideButton() {
+	$('#startButton').hide();
+}
+// Links "Meet the Furry Friends" button to a variable
+var startButton = document.getElementById('startButton');
+
+// Game begins as soon as "Meet the Furry Friends" button is clicked
+startButton.addEventListener('click', countDown);
+startButton.addEventListener('click', hideButton);
+
+});
